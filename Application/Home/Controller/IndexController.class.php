d6dbe95440f1e6783772895643b02cbc11b03f43
jizhuang <jizhuangmm@163.com>
2018-01-29 14:42:26 +0800

    人脸识别

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,108 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        $this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px } a,a:hover{color:blue;}</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>版本 V{$Think.version}</div><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_55e75dfae343f5a1"></thinkad><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
+        // 图片base64编码
+        $path   = '/path/to/image';
+        $data   = file_get_contents($path);
+        $base64 = base64_encode($data);
+
+        // 设置请求数据
+        $appkey = 'a95eceb1ac8c24ee28b70f7dbba912bf';
+        $params = array(
+            'app_id'     => '1000001',
+            'image'      => $base64,
+            'mode'       => '0',
+            'time_stamp' => strval(time()),
+            'nonce_str'  => strval(rand()),
+            'sign'       => '',
+        );
+        $params['sign'] = $this->getReqSign($params, $appkey);
+
+        // 执行API调用
+        $url = 'https://api.ai.qq.com/fcgi-bin/face/face_detectface';
+        $response = $this->doHttpPost($url, $params);
+        echo $response;
     }
+
+    // getReqSign ：根据 接口请求参数 和 应用密钥 计算 请求签名
+    // 参数说明
+    //   - $params：接口请求参数（特别注意：不同的接口，参数对一般不一样，请以具体接口要求为准）
+    //   - $appkey：应用密钥
+    // 返回数据
+    //   - 签名结果
+    function getReqSign($params /* 关联数组 */, $appkey /* 字符串*/)
+    {
+        // 1. 字典升序排序
+        ksort($params);
+
+        // 2. 拼按URL键值对
+        $str = '';
+        foreach ($params as $key => $value)
+        {
+            if ($value !== '')
+            {
+                $str .= $key . '=' . urlencode($value) . '&';
+            }
+        }
+
+        // 3. 拼接app_key
+        $str .= 'app_key=' . $appkey;
+
+        // 4. MD5运算+转换大写，得到请求签名
+        $sign = strtoupper(md5($str));
+        return $sign;
+    }
+
+    // doHttpPost ：执行POST请求，并取回响应结果
+    // 参数说明
+    //   - $url   ：接口请求地址
+    //   - $params：完整接口请求参数（特别注意：不同的接口，参数对一般不一样，请以具体接口要求为准）
+    // 返回数据
+    //   - 返回false表示失败，否则表示API成功返回的HTTP BODY部分
+    function doHttpPost($url, $params)
+    {
+        $curl = curl_init();
+
+        $response = false;
+        do
+        {
+            // 1. 设置HTTP URL (API地址)
+            curl_setopt($curl, CURLOPT_URL, $url);
+
+            // 2. 设置HTTP HEADER (表单POST)
+            $head = array(
+                'Content-Type: application/x-www-form-urlencoded'
+            );
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $head);
+
+            // 3. 设置HTTP BODY (URL键值对)
+            $body = http_build_query($params);
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $body);
+
+            // 4. 调用API，获取响应结果
+            curl_setopt($curl, CURLOPT_HEADER, false);
+            curl_setopt($curl, CURLOPT_NOBODY, false);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, true);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+            $response = curl_exec($curl);
+            if ($response === false)
+            {
+                $response = false;
+                break;
+            }
+
+            $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+            if ($code != 200)
+            {
+                $response = false;
+                break;
+            }
+        } while (0);
+
+        curl_close($curl);
+        return $response;
+    }
+
 }
